474834f4b843fee0de00d907ae21daebbda31208
Aaronginnypanikulam <53616714+Aaronginnypanikulam@users.noreply.github.com>
2020-02-18 17:55:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas,backgroundImage,playerCount,database,form,player,game;
+var gameState = 0;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+ 
+}
+
+function draw(){
+ 
+  
+}
+
